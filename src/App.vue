8fb72b30ed9448c1fdc9768ca0458d92cc0277cb
yatharth17 <yatharthkatyal4@gmail.com>
2020-04-23 08:30:59 +0530

    final-commit

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,48 @@
 <template>
   <div id="app">
-   <Todos v-bind:todos="todos"/>
+    <Header/>
+    <AddTodo v-on:add-todo="addTodo"/>
+   <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-
+import Header from './layout/Header'
+import AddTodo from './layout/AddTodo'
 import Todos from './components/Todos'
+import axios from 'axios'
 export default {
   name: 'App',
   components: {
-    Todos
+    Todos,
+    Header,
+    AddTodo
   },
   data(){
     return{
       todos:[
-        {
-         id:1,
-         title:"Todo One",
-         completed:false
-        },
-        {
-         id:2,
-         title:"Todo Two",
-         completed:true
-        },
-        {
-         id:1,
-         title:"Todo Three",
-         completed:false
-        }
-
       ]
     }
-  }
+  },
+  methods:{
+    deleteTodo(id){
+      this.todos=this.todos.filter(todo=>todo.id!=id)
+      },
+      addTodo(newTodo){
+        const {title,completed}=newTodo
+        axios.post('https://jsonplaceholder.typicode.com/todos',{
+          title,
+          completed
+        }).then(res=>this.todos=[...this.todos,res.data])
+          .catch(err=>console.log(err))
+      }
+  },
+   created() {
+        console.log('hey')
+        axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+        .then(res=>this.todos=res.data)
+        .catch(err=>console.log(err))
+      }
 }
 </script>
 
@@ -47,5 +56,16 @@ export default {
   body{
     font-family: Arial, Helvetica, sans-serif;
     line-height: 1.4;
+  }
+    .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background: #666;
   }
 </style>
---
+++ src/layout/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="header">
+      <h1>TodoList</h1>
+  </header>
+</template>
+
+<script>
+export default {
+   name:'Header'
+}
+</script>
+
+<style scoped>
+.header{
+    background:rgb(34, 34, 34);
+    color: white;
+    text-align: center;
+    padding: 10px;
+}
+.header a{
+    color:white;
+    padding-right: 5px;
+}
+</style>
